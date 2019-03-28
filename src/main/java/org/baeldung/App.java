c5ac0283726160d0315ed765a2f0742bc146a779
MadushanDinusha <djabc.pwc@mail.com>
2019-03-28 15:54:57 +0530

    New:Application is ready to run

--- src/main/java/org/baeldung/App.java
+++ src/main/java/org/baeldung/App.java
@@ -1,13 +1,19 @@
 package org.baeldung;
 
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
 }
